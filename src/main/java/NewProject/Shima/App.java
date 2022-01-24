d3475fb69ee57d78ef19a96fdc6369a809ab31b3
liyanaerfan <lamisaliyana@gmail.com>
2022-01-23 21:19:12 -0600

    hello

---
+++ src/main/java/NewProject/Shima/App.java
@@ -0,0 +1,13 @@
+package NewProject.Shima;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
